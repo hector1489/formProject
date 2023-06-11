77cc1015ffeda7d1f62cd74b3f0806442e4d1a42
Leonardo Cifuentes <leonardocifuent@gmail.com>
2023-06-11 17:09:32 -0400

    Avances 11 de Junio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import Register from './components/Register/Register'
 
 import './App.css'
 
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
     <>
-    <div class="d-flex justify-content-center align-self-center">
+    <div className="d-flex justify-content-center align-self-center">
      <Register/>
      </div>
     </>
